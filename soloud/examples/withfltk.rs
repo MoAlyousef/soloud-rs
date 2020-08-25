8c37ed280df62ec7401e7a87a507a8eea6b3e0c3
Mohammed Alyousef <may642_2000@hotmail.com>
2020-08-25 23:41:09 +0300

    remove unused frame

--- soloud/examples/withfltk.rs
+++ soloud/examples/withfltk.rs
@@ -4,7 +4,6 @@
 // fn main() {
 //     let app = App::default();
 //     let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
-//     // let mut frame = Frame::new(0, 0, 400, 200, "");
 //     let mut but = Button::new(160, 210, 80, 40, "Click me!");
 
 //     let mut sl = soloud::Soloud::new().unwrap();
