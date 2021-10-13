f1d72e057c4be25e406344d439ede2db35593c27
Mohammed Alyousef <may642_2000@hotmail.com>
2021-10-13 13:55:22 +0300

    add sys_simple example

---
+++ soloud-sys/examples/sys_simple.rs
@@ -0,0 +1,21 @@
+use soloud_sys::soloud::*;
+
+fn main() {
+    unsafe {
+        let sl = Soloud_create();
+        Soloud_init(sl);
+        Soloud_setGlobalVolume(sl, 3.0);
+    
+        let wav = Wav_create();
+    
+        let ret = Wav_load(wav, "覆.mp3\0".as_ptr() as _);
+    
+        dbg!(ret);
+
+        Soloud_play(sl, wav);
+        while Soloud_getVoiceCount(sl) > 0 {
+            // calls to play are non-blocking, so we put the thread to sleep
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+}
