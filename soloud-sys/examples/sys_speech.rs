6f26d6c41dd60c333365b508fd3731dee78feba2
Mohammed Alyousef <may642_2000@hotmail.com>
2021-10-30 15:40:58 +0300

    add sys_speech example

---
+++ soloud-sys/examples/sys_speech.rs
@@ -0,0 +1,22 @@
+use soloud_sys::soloud::*;
+
+fn main() {
+    unsafe {
+        let sl = Soloud_create();
+        Soloud_init(sl);
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        Soloud_setGlobalVolume(sl, 3.0);
+    
+        let speech = Speech_create();
+    
+        let ret = Speech_setText(speech, "Hello World\0".as_ptr() as _);
+    
+        dbg!(ret);
+
+        Soloud_play(sl, speech);
+        while Soloud_getVoiceCount(sl) > 0 {
+            // calls to play are non-blocking, so we put the thread to sleep
+            std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+}
